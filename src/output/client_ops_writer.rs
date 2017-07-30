d69d6c50f0a8110be921cda50eb5606e16cbe719
Timothy Meade <zt.tmzt@gmail.com>
2017-07-30 13:12:51 -0400

    Remove unused code in client_ops_writer

--- src/output/client_ops_writer.rs
+++ src/output/client_ops_writer.rs
@@ -22,8 +22,6 @@ pub struct ElementOpsWriter<'input: 'scope, 'scope> {
     pub doc: &'input DocumentState<'input>,
     pub stream_writer: &'scope mut ElementOpsStreamWriter,
     pub scopes: LinkedHashMap<String, ElementOpScope>,
-    // pub scopes: LinkedHashMap<String, ScopePrefixes>,
-    // pub expr_scopes: LinkedHashMap<String, ExprScopeProcessingState>,
     pub blocks: BlockMap,
     pub cur_block_id: Option<String>
 }
@@ -90,11 +88,7 @@ impl<'input: 'scope, 'scope> ElementOpsWriter<'input, 'scope> {
                 &ElementOp::ElementVoid(ref element_tag, ref element_key, ref attrs, ref events) => {
                     let scope = self.scopes.back().map_or(scope.clone(), |s| s.1.clone());
 
-                    // let attrs = attrs.as_ref().map(|attrs| attrs.clone().iter());
-                    // let events = events.as_ref().map(|events| events.clone().iter());
-
                     let element_key = element_key.as_ref().map_or("null", |s| s);
-                    // let element_key = self.scope_prefix(scope_prefix, element_key);
 
                     let attrs = attrs.as_ref().map(|attrs| attrs.iter());
                     let events = events.as_ref().map(|events| events.iter());
@@ -105,13 +99,11 @@ impl<'input: 'scope, 'scope> ElementOpsWriter<'input, 'scope> {
                 &ElementOp::ElementClose(ref element_tag) => {
                     let scope = self.scopes.back().map_or(scope.clone(), |s| s.1.clone());
 
-                    // let scope = self.scopes.scope().unwrap_or(containing_scope);
                     self.stream_writer.write_op_element_close(w, op, doc, &scope, element_tag)?;
                 }
                 &ElementOp::WriteValue(ref expr, ref value_key) => {
                     let scope = self.scopes.back().map_or(scope.clone(), |s| s.1.clone());
 
-                    // let scope = self.scopes.scope().unwrap_or(containing_scope);
                     let value_key = value_key.as_ref().map_or("null", |s| s);
                     self.stream_writer.write_op_element_value(w, op, doc, &scope, expr, value_key)?;
                 }
@@ -120,14 +112,11 @@ impl<'input: 'scope, 'scope> ElementOpsWriter<'input, 'scope> {
                                             ref props,
                                             ref lens) => {
                     let scope = self.scopes.back().map_or(scope.clone(), |s| s.1.clone());
-                    // let expr_scope = self.expr_scopes.back().map_or(expr_scope.clone(), |s| s.1.clone());
 
                     let comp = doc.comp_map.get(component_ty.as_str());
                     if let Some(ref comp) = comp {
-//                        let props: Option<Vec<Prop>> = props.as_ref().map(|c| c.iter().map(Clone::clone).collect());
                         let lens = lens.as_ref().map(|s| s.as_str());
 
-                        // let component_id = comp.as_ref().map(|c| c.component_id.clone());
                         let component_key = component_key.as_ref().map_or("null", |s| s);
                         let component_id = format!("{}_1", component_key);
 
@@ -209,25 +198,4 @@ impl<'input: 'scope, 'scope> ElementOpsWriter<'input, 'scope> {
 
         Ok(())
     }
-
 }
-
-// pub trait ComponentStreamWriter<'input> {
-//     fn write_js_event_bindings(&self, w: &mut io::Write, events_iter: Iter<EventsItem>, scope_prefix: Option<&ScopePrefixType>) -> Result;
-//     fn write_store_definition(&mut self, w: &mut io::Write, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>) -> Result;
-//     fn write_component_definitions(&mut self, w: &mut io::Write, comp: &'input Iter<Component>, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>) -> Result;
-//     fn write_component_definition(&mut self, w: &mut io::Write, comp: &Component, doc: &'input DocumentState, scope_prefix: Option<&ScopePrefixType>) -> Result;
-// }
-
-// pub trait ReducerActionStreamWriter<'input> {
-//     fn write_reducer_action(&mut self, w: &mut io::Write, reducer_key: &'input str, reducer_data: &'input ReducerKeyData, action_data: &'input ReducerActionData, action_ty: Option<&str>, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>) -> Result;
-// }
-
-// pub trait JsValueStreamWriter<'input> {
-//     fn write_js_var_reference(&mut self, w: &mut io::Write, var_name: Option<&str>, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>) -> Result;
-//     fn write_js_expr_value(&mut self, w: &mut io::Write, node: &ExprValue, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>) -> Result;
-//     fn write_js_props_object(&mut self, w: &mut io::Write, props: Option<Iter<'input, Prop>>, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>)-> Result;
-//     fn write_js_incdom_attr_array(&mut self, w: &mut io::Write, attrs: Option<Iter<'input, Prop>>, doc: &DocumentState, scope_prefix: Option<&ScopePrefixType>, base_key: Option<&str>) -> Result;
-// }
-
-// impl<'input: 'scope, 'scope, T, S> WriteOpsContent for S where S: ElementOpsWriter<'input, Output = T> where T: ContentWriter {
