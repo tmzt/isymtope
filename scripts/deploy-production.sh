49a4e5c0edd0961dc8f21d38ae841fe9ba0f1475
Timothy Meade <zt.tmzt@gmail.com>
2018-02-18 22:29:55 -0500

    Update .travis.yml

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -12,7 +12,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
+gcloud docker -- push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
 
