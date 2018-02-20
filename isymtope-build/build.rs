b23ddf4c2fd4fe982634f54d16f1a61e2af63fdf
Timothy Meade <zt.tmzt@gmail.com>
2018-02-19 23:44:04 -0500

    Rebuild parser.rs each time, move to OUT_DIR and fix build script for lalrpop 0.14.0

--- isymtope-build/build.rs
+++ isymtope-build/build.rs
@@ -1,17 +1,24 @@
+extern crate lalrpop;
 extern crate ructe;
 
-use ructe::compile_templates;
 use std::env;
 use std::path::PathBuf;
 
+use ructe::compile_templates;
+use lalrpop::Configuration;
+
 fn main() {
+    // Build internal (html) templates
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let in_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
         .join("res/templates/page");
     compile_templates(&in_dir, &out_dir).expect("foo");
 
-    // lalrpop::Configuration::new()
-    //     .emit_comments(true)
-    //     .process_dir("./src/input/parser")
-    //     .unwrap();
+    // Build parser
+    Configuration::new()
+        .emit_comments(true)
+        .use_cargo_dir_conventions()
+        .process()
+        .ok()
+        .unwrap();
 }
