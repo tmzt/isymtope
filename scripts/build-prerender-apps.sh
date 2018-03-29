4e2e2cf61985beed2f80e169c9718e7a71868eeb
Timothy Meade <zt.tmzt@gmail.com>
2018-03-29 16:58:09 -0400

    Update .travis.yml: use ./scripts/build-prerender-apps.sh

--- scripts/build-prerender-apps.sh
+++ scripts/build-prerender-apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 readonly ROOT=$(realpath $( dirname "${BASH_SOURCE[0]}")/..)
-readonly APPS_BASE="http://localhost:3000/app/playground/_worker/app"
+readonly APPS_BASE=${WORKER_APP_BASE_URL:-http://localhost:3000/app/playground/_worker/app}
 
 function log() {
     echo $1 > /dev/stderr
@@ -9,13 +9,14 @@ function log() {
 
 function prerender() {
     local APP_NAME=$1
-    log "Building ${APP_NAME} prerender..."
-    APP_DIR=./res/tests/app DEFAULT_APP=$APP_NAME ../target/x86_64-unknown-linux-musl/debug/isymtope-cli --base-url $APPS_BASE/todomvc --output ./res/tests/app/${APP_NAME}/index.html /app.ism
+    log "Building ${APP_NAME} prerender with base_url (${APPS_BASE})..."
+    APP_DIR=./res/tests/app DEFAULT_APP=$APP_NAME ../target/x86_64-unknown-linux-musl/debug/isymtope-cli --base-url $APPS_BASE/todomvc --output ./res/tests/app/${APP_NAME}/index.html /app.ism 2>/dev/null
 }
 
 pushd ${ROOT}/isymtope-server
     log "Building isymtope-cli..."
     (cd ../isymtope-cli && cargo build --target x86_64-unknown-linux-musl) && \
+        prerender playground && \
         prerender todomvc && \
         prerender shopping
 popd
