840866b7aec383371579acb9f8b55b7d1149a2b0
Timothy Meade <zt.tmzt@gmail.com>
2018-04-06 02:17:24 -0400

    Remove errors for token.rs completely

--- isymtope-build/src/input/parser/token.rs
+++ isymtope-build/src/input/parser/token.rs
@@ -1,48 +1,3 @@
-use std::error::Error;
-use std::fmt;
-use std::result::Result;
-
-// use error::*;
-
-// #[derive(Debug)]
-// pub enum TemplateParseError {
-//     Unexpected(usize),
-//     UnterminatedString(usize),
-//     InvalidNumber(usize),
-// }
-
-// impl Error for TemplateParseError {
-//     fn description(&self) -> &str {
-//         match self {
-//             &TemplateParseError::Unexpected(_) => "Unexpected token",
-//             &TemplateParseError::UnterminatedString(_) => "Unterminated string",
-//             &TemplateParseError::InvalidNumber(_) => "Invalid number",
-//         }
-//     }
-// }
-
-// impl fmt::Display for TemplateParseError {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         match self {
-//             &TemplateParseError::Unexpected(ref s) => write!(f, "Unexpected token at pos {0}", s),
-//             &TemplateParseError::UnterminatedString(ref s) => {
-//                 write!(f, "Unterminated string at start {0}", s)
-//             }
-//             &TemplateParseError::InvalidNumber(ref s) => {
-//                 write!(f, "Invalid number at start {0}", s)
-//             }
-//         }
-//     }
-// }
-
-// impl Into<ParsingError> for TemplateParseError {
-//     fn into(self) -> ParsingError {
-//         let description = self.description();
-//         ParsingError::new(description.to_owned())
-//     }
-// }
-
-#[allow(dead_code)]
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
     UseKeyword,
@@ -119,5 +74,3 @@ pub enum Token {
     LiteralBool(bool),
     VariableReference(String),
 }
-
-// pub type TemplateParseResult<T> = ::std::result::Result<T, TemplateParseError>;
